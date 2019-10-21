5cc7414ea9735ee7ad6eed45b7e5429e20dedef9
RKNyra <rk.nyra@gmail.com>
2019-10-21 11:54:11 +0300

    create a config file with configuration classes that are to be used in the app creation(dev and prod) stages

---
+++ config.py
@@ -0,0 +1,23 @@
+class Config:
+    '''
+    General configurations (configs) parent class
+    '''
+    pass
+
+class ProdConfig(Config):
+    '''
+    Production configs child class
+    
+    Args:
+        Config: The parent config class with General configs settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configs child class
+    
+    Args:
+        Config: The parent config class with General configs settings
+    '''
+    DEBUG = True
